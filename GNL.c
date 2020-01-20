9a1f7748ff93053e0684ad40d8c3ff1642df5620
Antonio Garzon iniesta <agarzon-@c3r8s6.42madrid.com>
2020-01-20 17:25:03 +0100

    20m

--- GNL.c
+++ GNL.c
@@ -1,104 +1,123 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define BUFFER_SIZE 128
+#define BUFFER_SIZE 42
 
-int ft_len_until_c(char *s, char c)
+int ft_strlen_until(char *s, char c)
 {
-    int i = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-            return (i);
-        i++;
-    }
-    if (s[i] == c)
-        return (i);
-    return (-1);
+	int l = 0;
+	while(s[l] != '\0')
+	{
+		if (s[l] == c)
+			return(l);
+		l++;
+	}
+	if (s[l] == c)
+		return(l);
+	return(-1);
 }
 
 char *ft_strjoin(char *s1, char *s2)
 {
-    char *str;
-    int i = 0, j = 0;
+	int l = 0;
+	int q = 0;
+	char *new;
+	int total;
 
-    if (!(str = malloc(sizeof(char) *
-        (ft_len_until_c(s1, '\0') + ft_len_until_c(s2, '\0') + 1))))
-        return (NULL);
-    while (s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        str[i + j] = s2[j];
-        j++;
-    }
-    free(s1);
-    str[i + j] = '\0';
-    return (str);        
+	total = ft_strlen_until(s1, '\0') + ft_strlen_until(s2, '\0');
+	new = malloc(sizeof(char) * total + 1);
+	while(s1[l] != '\0')
+	{
+		new[l] = s1[l];
+		l++;
+	}
+	while(s2[q] != '\0')
+	{
+		new[l + q] = s2[q];
+		q++;
+	}
+	new[l + q] = '\0';
+	return(new);
 }
 
 char *ft_substr(char *s, int start, int len)
 {
-    char *str;
-    int i = 0;
-    if (!s)
-        return (NULL);
-    if (start > ft_len_until_c(s, '\0'))
-        return (NULL);
-    if (!(str = malloc(sizeof(char) * len + 1)))
-        return (NULL);
-    while (s[start] && len)
-    {
-        str[i++] = s[start++];
-        len--;
-    }
-    str[i] = '\0';
-    return (str);    
+	char *new;
+	int l = 0;
+
+	if (start > ft_strlen_until(s, '\0'))
+		return(NULL);
+	if (!s)
+		return(NULL);
+	new = malloc(sizeof(char) * len + 1);
+	while (len > 0)
+	{
+		new[l] = s[start];
+		l++;
+		start++;
+		len--;
+	}
+	return(new);
 }
 
-static void		*ft_calloc(int count, int size)
+static void *ft_calloc(int size, int count)
 {
-	void	*mem;
-	int		total;
+	void *meme;
+	int total;
 
 	total = size * count;
-	if (!(mem = malloc(total)))
-		return (NULL);
-	while (total-- > 0)
+	meme = malloc (total);
+	while(total > 0)
 	{
-		*(unsigned char*)mem++ = 0;
+		*(unsigned char*)meme++ = 0;
+		total--;
 	}
-	return (mem - count * size);
+	return(meme - size * count);
+
 }
 
 int get_next_line(char **line)
 {
-    static char *rest;
-    char *tmp;
-    char buf[BUFFER_SIZE + 1];
-    int ret;
+	static char *str;
+	int br;
+	char buff[BUFFER_SIZE + 1];
+	char *tmp;
+	
+	if (!line)
+		return(-1);
+	if (!(str = ft_calloc(sizeof(char), 1)))
+		return(-1);
+	while(ft_strlen_until(str, '\n') < 0 && (br = read(0, buff, BUFFER_SIZE)) > 0)
+	{
+		buff[br] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	*line = ft_substr(str, 0, ft_strlen_until(str, '\n'));
+	printf("STR: %s\n", str);
+	if (ft_strlen_until(str, '\n') < 0)
+	{
+		free(str);
+		return(0);
+	}
+	tmp = str;
+	str = ft_substr(tmp, ft_strlen_until(tmp, '\n') + 1, ft_strlen_until(tmp, '\0'));
+	free(tmp);
+	return(1);
 
-    if (!line)
-        return (-1);
-    if (!rest && !(rest = ft_calloc(sizeof(char), 1)))
-        return (-1);
-    while (ft_len_until_c(rest, '\n') < 0 && (ret = read(0, buf, BUFFER_SIZE)) > 0)
-    {
-        buf[ret] = '\0';
-        rest = ft_strjoin(rest, buf);
-    }
-    *line = ft_substr(rest, 0, ft_len_until_c(rest, '\n'));
-    if (ft_len_until_c(rest, '\n') < 0)
-    {
-        free(rest);
-        return (0);    
-    }
-    tmp = rest;
-    rest = ft_substr(rest, ft_len_until_c(rest, '\n') + 1, ft_len_until_c(rest, '\0'));
-    free(tmp);
-    return (1);
 }
+
+
+int main (int argc, char **argv)
+{
+	int l;
+	char line[0][200] =	"hola que pasa";
+	l = get_next_line(line);
+	//	printf("%d\n", l);
+	l = get_next_line(argv);
+//	printf("%d\n", l);
+	l = get_next_line(argv);
+//	printf("%d\n", l);
+
+}
+
