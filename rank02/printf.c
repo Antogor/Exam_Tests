837369465eb5a97b701a34195c4af67875fef8b4
Antogor <antoniogarzoniniesta@gmail.com>
2020-06-30 13:58:17 +0200

    almost printf

--- rank02/printf.c
+++ rank02/printf.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 typedef struct s_print
 {
@@ -11,8 +12,17 @@ typedef struct s_print
 	
 } t_print;
 
+int	ft_strlen(char *s)
+{
+	int l;
 
-char *atoi_base(int bas, unsigned int nb)
+	l = 0;
+	while(s[l] != '\0')
+		l++;
+	return(l);
+}
+
+char *atoi_base(int bas, unsigned int nb, int *l)
 {
 	char *s;
 	unsigned int tmp;
@@ -28,9 +38,13 @@ char *atoi_base(int bas, unsigned int nb)
 		{
 			tmp = tmp / 10;
 			digits++;
+			*l += 1;
 		}
 		if (nb == 0)
+		{
 			digits = 1;
+			*l += 1;
+		}
 		s = (char *)malloc(sizeof(char) * digits + 1);
 		s[digits--] = '\0';
 		if (nb == 0)
@@ -49,9 +63,13 @@ char *atoi_base(int bas, unsigned int nb)
 		{
 			tmp = tmp / 16;
 			digits++;
+			*l += 1;
 		}
 		if (nb == 0)
+		{
 			digits = 1;
+			*l += 1;
+		}
 		s = (char *)malloc(sizeof(char) * digits + 1);
 		s[digits--] = '\0';
 		if (nb == 0)
@@ -66,42 +84,162 @@ char *atoi_base(int bas, unsigned int nb)
 	return (s);
 }
 
-int put_nb(int nb)
+int put_nb(int nb, int *l, int spaces, int precision)
 {
 	char *s;
-
+	int total = 0;
+	int total2 = 0;
+	int n = 0;
+	int len = 0;
+	
 	if (nb < 0)
 	{
 		nb *= -1;
-		write(1, "-", 1);
+		n = '-';
+		*l += 1;
 	}
-	s = atoi_base(10, nb);
-	write(1, s, strlen(s));
+	s = atoi_base(10, nb, l);
+	len += ft_strlen(s);
+	if (precision > len)
+	{
+		if (n == '-')
+		{
+			total = precision - len;
+			if (spaces > (total + len))
+				total2 = spaces - (total + len) - 1;
+		}
+		else
+		{
+			total = precision - len;
+			if (spaces > (total + len))
+				total2 = spaces - (total + len);
+		}
+	}
+	else
+		total2 = spaces - len;
+	while (total2 > 0)
+	{
+		write (1, " ", 1);
+		*l += 1;
+		total2--;
+	} 
+	if (n == '-')
+		write(1, "-", 1);
+	while (total > 0)
+	{
+		write (1, "0", 1);
+		*l += 1;
+		total--;
+	}
+	write(1, s, ft_strlen(s));
 	free(s);
 	return (0);
 }
 
-int put_hex(unsigned int nb)
+int put_hex(unsigned int nb, int *l, int spaces, int precision)
 {
 	char *s;
-
-	s = atoi_base(16, nb);
-	write(1, s, strlen(s));
+	int total = 0;
+	int total2 = 0;
+	
+	s = atoi_base(16, nb, l);
+	int len = ft_strlen(s);
+	if (precision > len)
+	{
+		total = precision - len;
+		if (spaces > (total + len))
+			total2 = spaces - (total + len);
+	}
+	else
+		total2 = spaces - len;
+	while (total2 > 0)
+	{
+		write (1, " ", 1);
+		*l += 1;
+		total2--;
+	} 
+	while (total > 0)
+	{
+		write (1, "0", 1);
+		*l += 1;
+		total--;
+	} 
+	write(1, s, ft_strlen(s));
 	free(s);
 	return (0);
+}
+
+int	ft_printf(char *s, ...)
+{
+	va_list args;
+	char *s1;
+	int nb;
+	t_print p;
+	int l = 0;
+	
+	p.len = 0;
+	p.spaces = 0;
+	p.precision = 0;
+	va_start(args, s);
+	while(s[l] != '\0')
+	{
+		if (s[l] != '%')
+		{
+			write(1, &s[l], 1);
+			p.len++;
+			l++;
+		}
+		else
+		{
+			l++;
+			while(s[l] >= '0' && s[l] <= '9')
+			{
+				p.spaces = p.spaces * 10 + (s[l] - 48);
+				l++;
+			}
+			if (s[l] == '.')
+			{
+				l++;
+				while(s[l] >= '0' && s[l] <= '9')
+				{
+					p.precision = p.precision * 10 + (s[l] - 48);
+					l++;
+				}
+			}
+			if (s[l] == 'd')
+			{
+				l++;
+				put_nb(va_arg(args, int), &p.len, p.spaces, p.precision);
+			}
+			else if (s[l] == 'x')
+			{
+				l++;
+				put_hex(va_arg(args, unsigned int), &p.len, p.spaces, p.precision);
+			}
+			else if (s[l] == 's')
+			{
+				l++;
+				//put_nb(va_arg(args, int), &p.len);
+			}
+			p.spaces = 0;
+			p.precision = 0;
+		}
+	}
+	va_end(args);
+	return(p.len);
 }
 
 int main (void)
 {
-	char *s1;
 	int nb;
+	int m;
+	int o;
 
-	nb = 423442;
-	put_nb(nb);
-	printf("\n");
-	put_hex(nb);
-	printf("\n");
-	printf("DEC_ORI: %d\n", nb);
-	printf("HEXA_ORI: %x\n", nb);
+	nb = -4231334;
+	m = ft_printf("DEC_ORI: %25.16d HEX_MIO: %25.16x\n", nb, nb);
+	o = printf("DEC_ORI: %25.16d HEX_ORI: %25.16x\n", nb, nb);
+//	m = ft_printf("%5.2d\n", nb);
+//	o = printf("%5.2d\n", nb);
+	printf("MIO: %d ORI: %d\n", m, o);
 	return (0);
 }
